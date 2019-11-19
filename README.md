dbfa077ed7e22bcc00f530c84db2a0dafbb1f5c7
James Deadman <jimdeadman@hotmail.com>
2019-11-18 23:30:33 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 "# SillyMonths" 
+This was a challenge to find the most rediculous way to round to the nearest month 1, 3, 5, 7.
+
+Nothing to see here.
---
+++ SillyMonths/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SillyMonthsML.Model;
+
+namespace SillyMonths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                var input = new ModelInput() { Input = i };
+                ModelOutput result = ConsumeModel.Predict(input);
+                Console.WriteLine($"{i} => {result.Prediction}");
+            }
+        }
+    }
+}
